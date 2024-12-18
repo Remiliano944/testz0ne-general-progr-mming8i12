9f44534d005774860cd6e6f8382164e9ccb21d40
Remiliano <emilianoprestesdacosta@proton.me>
2024-12-18 05:58:29 -0300

    Robi

---
+++ CSnRuby/try.rb
@@ -0,0 +1,21 @@
+#puts "Insert base"
+#rectbase = gets
+#puts "Insert height"
+#rectheight = gets
+#
+#puts "the resulting area is: " + (rectbase.to_i * rectheight.to_i).to_s
+#
+
+
+#z = 8
+#x = 7
+#y = "9"
+#
+#puts (((z * z) * y ^ 3)/ x)
+
+#outputs 2.5
+#puts (5 / (2 * 1.0))
+#outputs 2.0
+#puts (5 / 2 * 1.0)
+#outputs ... 3.0!
+#puts(Math.sqrt(9))
